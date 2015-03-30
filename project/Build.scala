74fd79bad973165d3a98dc69635858a0c150afa7
Sander Ufkes <sander@lunatech.com>
2015-03-30 10:05:35 +0200

    Renamed app to play-googleopenconnect

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "googleopenconnect"
+  val appName         = "play-googleopenconnect"
   val appVersion      = "1.0"
 
   val appDependencies = Seq(
